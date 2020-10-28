2a9ae877e5878cbef7faa54a6c5cbb818811a6e4
Charles Wyatt <cwdh4@mail.umsl.edu>
2020-10-28 11:52:57 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define CHARMAX 1024
+
+int main(int argc, char *argv[])
+{
+	char pPos;
+
+        if (argc > 2)
+        {
+                printf("See readme for usage details\nThere shouldn't be more than two argument$
+                return 0;
+        }
+
